24bae6e25856d657782e350505716805e330ad60
zulamrifi <135478325+zulamrifi@users.noreply.github.com>
2026-04-23 18:14:41 +0800

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,11 +2,12 @@
 // FIREBASE INIT — public/firebase-init.js
 // ⚠️  GANTI NILAI DI BAWAH DENGAN KONFIGURASI FIREBASE ANDA
 // ============================================================
-const FIREBASE_CONFIG = {
-  apiKey:            "GANTI_DENGAN_API_KEY_ANDA",
-  authDomain:        "NAMA-PROJECT.firebaseapp.com",
-  projectId:         "NAMA-PROJECT",
-  storageBucket:     "NAMA-PROJECT.appspot.com",
-  messagingSenderId: "000000000000",
-  appId:             "1:000000000000:web:000000000000000000000000"
+const firebaseConfig = {
+  apiKey: "AIzaSyA95tw-ECPzfIH9eJmi3VxFYoGzRQdiwWM",
+  authDomain: "website-kuasekongkang.firebaseapp.com",
+  databaseURL: "https://website-kuasekongkang-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "website-kuasekongkang",
+  storageBucket: "website-kuasekongkang.firebasestorage.app",
+  messagingSenderId: "757740985056",
+  appId: "1:757740985056:web:fc85a3e18b17f9413d8e85"
 };
